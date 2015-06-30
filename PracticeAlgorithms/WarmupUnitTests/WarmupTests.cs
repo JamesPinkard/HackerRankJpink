deb984dd113772a915983e09160ae62723c6b629
JamesPinkard <James.b.Pinkard@gmail.com>
2015-06-30 16:59:08 -0500

    Added GetDifference Method

--- PracticeAlgorithms/WarmupUnitTests/WarmupTests.cs
+++ PracticeAlgorithms/WarmupUnitTests/WarmupTests.cs
@@ -12,10 +12,28 @@ namespace WarmupUnitTests
     [TestFixture]
     public class WarmupTests
     {
+        string[] testString = new string[] {"3 5 7", "4 9 2","5 2 0"};
 
+        [Test]
         public void ReadIntoGrid_YieldsArray()
         {
+            // Setup
+
+            int[,] grid = DiagonalSums.ReadIntoGrid(testString, 3, 3);
+
+            Assert.AreEqual(0, grid[2, 2]);
+            Assert.AreEqual(3, grid.GetLength(1));
+        }
+
+        [Test]
+        public void GetDiagonalDifference_YieldsDifference()
+        {
+            int[,] grid = DiagonalSums.ReadIntoGrid(testString, 3, 3);
+
+            int difference = DiagonalSums.GetDiagonalDifference(grid);
+
             
+            Assert.AreEqual(21 - 12, difference);
         }
     }
 }
