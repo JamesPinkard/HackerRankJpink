afe5d86447841dfcccbaed4b757a48d52dbc61d1
JamesPinkard <James.b.Pinkard@gmail.com>
2015-07-02 20:47:11 -0500

    finished PlusMinus

--- PracticeAlgorithms/WarmupProblems/PlusMinus.cs
+++ PracticeAlgorithms/WarmupProblems/PlusMinus.cs
@@ -17,6 +17,53 @@ namespace WarmupProblems
             int negatives = getNegatives(myNumbers);
             int zeroes = getZeroes(myNumbers);
             int positivies = getPositives(myNumbers);
+
+            double negFrac = Math.Round((double)negatives / numberCount, 3);
+            double zeroFrac = Math.Round((double)zeroes / numberCount, 3);
+            double posFrac = Math.Round((double)positivies / numberCount, 3);
+
+            Console.WriteLine(posFrac);
+            Console.WriteLine(negFrac);
+            Console.WriteLine(zeroFrac);
+        }
+
+        private static int getNegatives(int[] myNumbers)
+        {
+            int count = 0;
+            foreach (var num in myNumbers)
+            {
+                if (num<0)
+                {
+                    count += 1;
+                }
+            }
+            return count;
+        }
+
+        private static int getZeroes(int[] myNumbers)
+        {
+            int count = 0;
+            foreach (var num in myNumbers)
+            {
+                if (num == 0)
+                {
+                    count += 1;
+                }
+            }
+            return count;
+        }
+
+        private static int getPositives(int[] myNumbers)
+        {
+            int count = 0;
+            foreach (var num in myNumbers)
+            {
+                if (num > 0)
+                {
+                    count += 1;
+                }
+            }
+            return count;
         }
     }
 }
