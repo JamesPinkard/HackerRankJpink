e7002d8cc63e8abbc51b402cbd87fcec91ebff02
JamesPinkard <James.b.Pinkard@gmail.com>
2015-07-20 12:17:15 -0500

    added recursion subset example

---
+++ PracticeAlgorithms/SearchProblems/MaxModuloSubset.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchProblems
+{
+    class MaxModuloSubset
+    {
+
+        // Practice 2
+        // Check through every possible subset in a list of numbers
+        // Return true if the sum of a subset equals the target sum
+        static bool CanMakeSum(List<int> nums, int targetSum)
+        {
+            List<int> soFar = new List<int>();
+            return RecMakeSum(soFar, nums, targetSum);
+        }
+
+        static bool RecMakeSum(List<int> soFar, List<int> nums, int targetSum)
+        {
+            if (nums.Count == 0)
+            {
+                return soFar.Sum() == targetSum;
+            }
+            else
+            {
+                List<int> nextNums = new List<int>();
+                nextNums = nums.GetRange(1, nums.Count - 1);
+
+                List<int> nextSoFar = new List<int>();
+                nextSoFar.AddRange(soFar);
+                nextSoFar.Add(nums[0]);
+
+                if (RecMakeSum(nextSoFar, nextNums, targetSum)) return true;
+                if (RecMakeSum(soFar, nextNums, targetSum)) return true;
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
