cb3c8d8e61efeb7ec32d7f8c4add3f5749a74676
JamesPinkard <James.b.Pinkard@gmail.com>
2015-07-22 19:17:43 -0500

    Implemented Heap data structure

---
+++ CoreAlgorithms/DataStructures/IHeap.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public interface IHeap<T>
+        where T : class
+    {
+        // Methods
+        T ExtractRoot();
+        
+        void Insert(T element);
+        void Clear();
+        void Remove(T element);
+        bool Contains(T element);
+
+        // Properties
+        T Root { get; }
+        int Count { get; }
+
+    }
+}
---
+++ PracticeAlgorithms/BytesAndBits/MaxXOR.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BytesAndBits
+{
+    class MaxXOR
+    {
+        static void Solve(string[] args)
+        {
+            int res;
+            int _l;
+            _l = Convert.ToInt32(Console.ReadLine());
+
+            int _r;
+            _r = Convert.ToInt32(Console.ReadLine());
+
+            res = maxXor(_l, _r);
+            Console.WriteLine(res);
+        }
+
+        static int maxXor(int l, int r)
+        {
+            int max = 0;
+
+            for (int i = l; i < r + 1; i++)
+            {
+                for (int j = i; j < r + 1; j++)
+                {
+                    int num = i ^ j;
+                    max = Math.Max(max, num);
+                }
+            }
+            return max;
+
+        }
+    }
+}
