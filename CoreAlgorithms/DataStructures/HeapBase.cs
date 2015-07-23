1390d851854ac534182e8d3894cce4cab25c870d
JamesPinkard <James.b.Pinkard@gmail.com>
2015-07-23 09:16:35 -0500

    Set up Heap Data structure

---
+++ CoreAlgorithms/DataStructures/HeapBase.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public abstract class HeapBase<T> : IHeap<T>
+        where T : class
+    {        
+        
+        private List<T> _elements = new List<T>();
+
+        protected abstract bool ElementCompare(T x, T y);
+
+        public T ExtractRoot()
+        {
+            T root = this.Root;
+            if (!isEmpty())
+            {
+                _elements.RemoveAt(0);
+            }
+            return root;
+        }
+
+        
+
+        // Methods
+
+        public void Insert(T element)
+        {
+            _elements.Add(element);
+            int index = _elements.Count - 1;
+            FloatUp(index);
+        }
+
+        protected void FloatUp(int index)
+        {       
+            int parent = Parent(index);
+            while (index > 0 && !ElementCompare(_elements[parent],_elements[index]))
+            {
+                swap(index, parent);
+                index = parent;
+            }
+        }
+
+        public void Clear()
+        {
+            _elements.Clear();
+        }
+
+        public void Remove(T element)
+        {
+            _elements.Remove(element);
+        }
+
+        public bool Contains(T element)
+        {
+            return _elements.Contains(element);
+        }
+
+        protected void FloatDown(int current)
+        {
+            int left = Left(current);
+            int right = Right(current);
+
+            int selected = selectTopIndex(left, current);
+            selected = selectTopIndex(right, selected);
+
+            if (selected != current)
+            {
+                swap(current, selected);
+                FloatDown(selected);
+            }
+        }
+
+        private void swap(int current, int other)
+        {
+            T temp = _elements[current];
+            _elements[current] = _elements[other];
+            _elements[other] = temp;
+        }
+
+        private int selectTopIndex(int left, int right)
+        {
+            int child = Math.Max(left, right);
+            int parent = Math.Min(left, right);
+
+            T childElement = _elements[child];
+            T parentElement = _elements[parent];
+
+            if (child <= _elements.Count && ElementCompare(childElement, parentElement))
+            {
+                return child;
+            }
+            else
+            {
+                return parent;
+            }
+            
+        }
+
+        protected int Parent(int index)
+        {
+            return index / 2;
+        }
+
+        protected int Left(int index)
+        {
+            return index * 2;
+        }
+
+        protected int Right(int index)
+        {
+            return (index * 2) + 1;
+        }
+
+        private bool isEmpty()
+        {
+            return _elements.Count <= 0;
+        }        
+
+        // Properties
+        public T Root
+        {
+            get
+            {
+                T result = default(T);
+                if (!isEmpty())
+                {
+                    result = _elements[0];
+                }
+                return result;
+            }
+        }
+
+        public int Count
+        {
+            get { return _elements.Count; }
+        }
+
+
+        protected Func<T, T, bool> ElementCompareFunc
+        {
+            get { return ElementCompare; }
+        }  
+    }
+}
---
+++ CoreAlgorithms/DataStructures/MinHeap.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    class MinHeap<T> : HeapBase<T> where T : class
+    {
+        public MinHeap() { }
+
+        
+        
+        public int Compare(T x, T y)
+        {
+            IComparable<T> myX = x as IComparable<T>;
+            return myX.CompareTo(y);
+        }
+
+        protected override bool ElementCompare(T x, T y)
+        {
+            return Compare(x, y) < 0;
+        }
+    }
+}
