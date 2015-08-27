83e1ffe9b2d3ba55a55de38d5f5f10c589659dfc
JamesPinkard <James.b.Pinkard@gmail.com>
2015-08-27 14:42:26 -0500

    AddedCsvReaderWriter

---
+++ CoreAlgorithms/ReaderWriter/CsvRow.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReaderWriter
+{
+    /// <summary>
+    /// Class to store one CSV row
+    /// </summary>
+    public class CsvRow : List<string>
+    {
+        public string LineText { get; set; }
+    }
+}
