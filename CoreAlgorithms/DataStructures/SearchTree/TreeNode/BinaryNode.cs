0f2d5749a2047aa9991d6ae5ee1b8203d855002f
JamesPinkard <James.b.Pinkard@gmail.com>
2015-09-02 16:47:06 -0500

    Added TreeNode

---
+++ CoreAlgorithms/DataStructures/SearchTree/TreeNode/BinaryNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.SearchTree.TreeNode
+{
+    public class BinaryNode<T>
+    {
+        public BinaryNode<T> RightChild { get; set; }
+        public BinaryNode<T> LeftChild { get; set; }
+        public T NodeValue { get; private set; }
+        public BinaryNode(T nodeValue)
+        {
+            NodeValue = nodeValue;
+        }
+
+        
+    }
+}
---
+++ CoreAlgorithms/DataStructures/SearchTree/TreeNode/TreeNode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class TreeNode<T>
+    {
+        public T NodeValue { get; private set; }
+        public TreeNode(T nodeValue)
+        {
+            NodeValue = nodeValue;
+        }
+
+        public List<TreeNode<T>> GetChildren()
+        {
+            return _children.ToList();
+        }
+
+        public void AddChild(TreeNode<T> value)
+        {
+            _children.Add(value);
+        }
+
+        private List<TreeNode<T>> _children;
+
+
+    }
+}
